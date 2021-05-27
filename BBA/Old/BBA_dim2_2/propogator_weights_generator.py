bf23ad4b2d0daa79344fc573c305871aab0961bc
jaulicino <50779175+jaulicino@users.noreply.github.com>
2021-05-26 23:14:06 -0500

    BBA 3

---
+++ BBA/Old/BBA_dim2_2/propogator_weights_generator.py
@@ -0,0 +1,858 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # prototyping LSS pipeline
+
+# ## setup
+
+# In[1]:
+
+
+# In[2]:
+
+
+import os, pickle, time, glob, sys, copy
+import numpy as np 
+import scipy
+import mdtraj as md 
+import MDAnalysis as mda
+import nglview as nv 
+from ipywidgets import interactive, VBox
+import sklearn.preprocessing as pre
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+import pyemma as py 
+from pyemma.util.contexts import settings
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[3]:
+
+
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1' 
+
+
+# In[4]:
+
+
+import tensorflow as tf
+import keras.backend as K
+from keras.models import load_model
+from keras.callbacks import EarlyStopping
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+K.tensorflow_backend.set_session(tf.Session(config=config))
+
+
+# In[5]:
+
+
+import sys
+sys.path.append('/home/alf/Desktop/LSS/FergLabLSS/LSS_components')
+#__all__ = ["hde", "propagator", "molgen"]
+from hde import *#__init__, hde, propagator, molgen, analysis #
+
+
+# ## locating trajectory data
+
+# In[6]:
+
+
+# DESRES Chignolin at 340 K
+# Lindorff-Larsen et al. Science 334 6055 517-520 (2011)
+# 200 ps time steps
+# 53 dcd files each conataining 10,000 frames at 200 ps time steps = 53*10,000*200 = 106 us
+
+timestep = 200 # ps
+
+pdb_dir = "/home/jaulicino/scratch-midway2/BBA/3_DESRES-Trajectory_1FME-1-protein/DESRES-Trajectory_1FME-1-protein/1FME-1-protein"
+pdb_file = os.path.join(pdb_dir, "system.pdb")
+
+trj_dir = pdb_dir
+trj_file = []
+for i in np.arange(0,10):
+    fname = "1FME-1-protein-" + str(i).zfill(3) + ".dcd"
+    trj_file.append(os.path.join(trj_dir, fname))
+print(pdb_file)
+
+
+# In[7]:
+
+
+print(pdb_file)
+view = nv.show_structure_file(pdb_file)
+view.clear_representations()
+view.add_ball_and_stick()
+view
+
+
+# ### mdtraj
+
+# In[8]:
+
+
+traj_obj = md.load(trj_file, top=pdb_file)
+
+
+# In[9]:
+
+
+traj_obj.center_coordinates(mass_weighted=False)
+traj_obj.superpose(traj_obj[0])
+
+
+# ## featurization
+
+# In[15]:
+
+
+# featurizing
+features = py.coordinates.featurizer(pdb_file)
+features.add_backbone_torsions(cossin=True)
+features.add_sidechain_torsions(which='all', cossin=True)
+atom_idx = features.select_Backbone() # select_Heavy() select_Ca()
+features.add_inverse_distances(atom_idx)
+
+#print(features.describe())
+print('dim = %d' % features.dimension())
+
+
+# In[16]:
+
+
+data = np.zeros((0,features.dimension()))
+for i in range(len(trj_file)):
+    q = py.coordinates.load(trj_file[i], features=features)
+    data = np.concatenate((data,q), axis=0)
+    print(i)
+    print(data.shape)
+
+
+# ## (1) latent space projection
+
+# ### parameters
+
+# In[17]:
+
+
+lag=50
+dim=5
+is_reversible=True
+
+
+# ### SRV
+
+# In[26]:
+
+
+#earlyStopping = EarlyStopping(monitor='val_loss', patience=30, verbose=1, mode='min', restore_best_weights=True)
+
+hde = HDE(
+    features.dimension(), 
+    n_components=dim, 
+    lag_time=lag,
+    reversible=is_reversible, 
+    n_epochs=300,
+    learning_rate=0.0005,
+    hidden_layer_depth=2,
+    hidden_size=100,
+    activation='tanh', 
+    batch_size=20000,
+    batch_normalization=True,
+    #callbacks=[earlyStopping], 
+    verbose=True
+)
+
+
+# In[27]:
+
+
+#hde.fit(data)
+
+
+# In[28]:
+
+
+#hde.callbacks = None
+#hde.history = None
+#pickle.dump(hde, open('hde.pkl', 'wb'), protocol=4)
+
+
+# In[18]:
+
+
+hde = pickle.load(open('bba_hde.pkl', 'rb'))
+
+
+# In[20]:
+
+
+hde_coords = hde.transform(data, side='left')
+#hde_coords_right = hde.transform(data, side='right')
+hde_timescales = hde.timescales_
+print(hde_timescales)
+print(hde_coords)
+#print(hde_coords_right)
+
+
+# ### SRV k-means clustering
+
+# #### n_cluster optimization using silhouette score
+# 
+# - expect n_cluster = (dim_kmeans+1) similar to PCCA (# macrostates = 1 + # singular vectors)
+# 
+# - Idea of inner simplex (PCCA) clustering directly in singular vectors of transfer operator:
+# F. Paul, H. Wu, M. Vossel, B.L. de Groot, and F. Noe "Identification of kinetic order parameters
+# for non-equilibrium dynamics" J. Chem. Phys. 150, 164120 (2019); doi: 10.1063/1.5083627
+# 
+# - k-means simpler and only for understanding of macrostates not construction of MSM
+
+# In[35]:
+
+
+dim_kmeans = 2
+hde_coords_kmeans = copy.deepcopy(hde_coords[:,:dim_kmeans])
+
+
+# In[36]:
+
+
+range_n_clusters = np.arange(2,5,1)
+silhouette_avg_array = []
+
+for n_clusters in range_n_clusters:
+    
+    # Initialize the clusterer with n_clusters value and a random generator
+    # seed of 10 for reproducibility.
+    clusterer = KMeans(n_clusters=n_clusters, random_state=200184)
+    cluster_labels = clusterer.fit_predict(hde_coords_kmeans)
+
+    # The silhouette_score gives the average value for all the samples.
+    # This gives a perspective into the density and separation of the formed
+    # clusters
+    silhouette_avg = silhouette_score(hde_coords_kmeans, cluster_labels)
+    print("For n_clusters =", n_clusters,
+          "The average silhouette_score is :", silhouette_avg)
+    silhouette_avg_array.append(silhouette_avg)
+
+
+# In[37]:
+
+
+n_clusters = dim_kmeans+1 # usually, but check silhouette scores
+
+
+# In[38]:
+
+
+clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+cluster_labels = clusterer.fit_predict(hde_coords_kmeans)
+centers = clusterer.cluster_centers_
+sample_silhouette_values = silhouette_samples(hde_coords_kmeans, cluster_labels)
+
+
+# #### visualizing configs proximate to k-means centers
+
+# In[44]:
+
+
+centers
+
+
+# In[45]:
+
+
+kmeans_viz = []
+for i in range(n_clusters):
+    idx_sort = np.argsort(np.sqrt(np.sum((hde_coords_kmeans - centers[i,:])**2, axis=1)))
+    kmeans_viz.append(idx_sort[:10])
+
+
+# In[47]:
+
+
+for i in range(n_clusters):
+    print(hde_coords_kmeans[kmeans_viz[i][0],:])
+
+
+# In[48]:
+
+
+clust_id = 0
+view = nv.NGLWidget()
+for k in range(len(kmeans_viz[clust_id])):
+    view.add_trajectory(traj_obj[kmeans_viz[clust_id][k]])
+view
+
+
+# In[49]:
+
+
+clust_id = 1
+view = nv.NGLWidget()
+for k in range(len(kmeans_viz[clust_id])):
+    view.add_trajectory(traj_obj[kmeans_viz[clust_id][k]])
+view
+
+
+# In[50]:
+
+
+clust_id = 2
+view = nv.NGLWidget()
+for k in range(len(kmeans_viz[clust_id])):
+    view.add_trajectory(traj_obj[kmeans_viz[clust_id][k]])
+view
+
+
+# ## (2) propagator
+
+# In[40]:
+
+
+dim_prop = 2
+
+
+# In[41]:
+
+
+traj_prop = copy.deepcopy(hde_coords[:,:dim_prop])
+
+
+# In[42]:
+
+
+prop_scaler = pre.MinMaxScaler(feature_range=(0,1))
+if dim_prop==1:
+    traj_prop_scaled = prop_scaler.fit_transform(traj_prop.reshape(-1, 1))
+else:
+    traj_prop_scaled = prop_scaler.fit_transform(traj_prop)
+
+
+# In[43]:
+
+
+n_mix = 25
+lag_prop = lag
+lr_prop = 0.0001
+
+
+# In[44]:
+
+
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(monitor='loss', patience=50, restore_best_weights=True)
+]
+
+
+# In[45]:
+
+
+prop = Propagator(
+    traj_prop_scaled.shape[1], 
+    n_components=n_mix, 
+    lag_time=lag_prop, 
+    batch_size=200000, 
+    learning_rate=lr_prop, 
+    n_epochs=20000,
+    callbacks=callbacks,
+    hidden_size=100,
+    activation='relu'
+)
+
+
+# In[58]:
+
+
+from hde.propagator import get_mixture_loss_func
+
+
+# In[59]:
+
+
+prop.model.compile(loss=get_mixture_loss_func(prop.input_dim, prop.n_components), optimizer=tf.keras.optimizers.Adam(lr=lr_prop))
+
+
+# In[60]:
+
+
+if is_reversible:
+    prop.fit([traj_prop_scaled, traj_prop_scaled[::-1]]) # reversible -- data augmentation with inverted trajectory
+else:
+    prop.fit(traj_prop_scaled)
+
+
+# In[62]:
+
+
+pickle.dump(prop.model.get_weights(), open('prop_weights.pkl', 'wb'))
+
+
+# In[46]:
+
+'''
+prop.model.set_weights(pickle.load(open('prop_weights.pkl', 'rb')))
+
+
+# In[62]:
+
+
+n_steps = np.int(np.floor(np.float(hde_coords.shape[0])/np.float(lag_prop)))
+n_traj = 5
+synth_trajs_scaled = [prop.propagate(traj_prop_scaled[0].reshape(1,-1).astype(np.float32), n_steps).reshape(n_steps, -1) for item in range(n_traj)]
+synth_trajs = [prop_scaler.inverse_transform(synth_trajs_scaled[i]) for i in range(n_traj)]
+
+
+# ## (3) generator
+
+# #### x_train = scaled latent space coordinates
+
+# In[65]:
+
+
+x_train = traj_prop_scaled
+
+
+# #### y_train = aligned molecular configurations
+
+# In[66]:
+
+
+ca_idx = traj_obj.top.select_atom_indices('alpha')
+traj_ca = traj_obj.atom_slice(ca_idx)
+
+
+# In[67]:
+
+
+traj_ca.superpose(traj_ca[0])
+
+
+# In[68]:
+
+
+view = nv.show_mdtraj(traj_ca[::lag_prop])
+#view.component_0.clear_representations()
+view.component_0.add_ribbon(color='blue', opacity=0.6)
+view
+view
+
+
+# In[69]:
+
+
+xyz = traj_ca.xyz.reshape(-1, traj_ca.n_atoms*3)
+
+
+# In[70]:
+
+
+xyz_scaler = pre.MinMaxScaler(feature_range=(-1,1))
+
+
+# In[71]:
+
+
+y_train = xyz_scaler.fit_transform(xyz)
+
+
+# In[72]:
+
+
+n_atoms = traj_ca.n_atoms
+
+
+# #### training cWGAN
+
+# In[73]:
+
+
+molgen = MolGen(
+    latent_dim=x_train.shape[1],
+    output_dim=y_train.shape[1],
+    batch_size=30000,
+    noise_dim=50,
+    n_epochs=2500,
+    hidden_layer_depth=2,
+    hidden_size=200,
+    n_discriminator=5
+)
+
+
+# In[7]:
+
+
+#molgen.fit(x_train, y_train)
+
+
+# In[87]:
+
+
+#molgen.fit(x_train, y_train) reason called twice?
+
+
+# In[84]:
+
+
+#molgen.generator.save('molgen_generator.h5')
+
+
+# In[85]:
+
+
+#molgen.discriminator.save('molgen_discriminator.h5')
+
+
+# In[89]:
+
+
+def swish(x):
+    return (K.sigmoid(x) * x)
+molgen.generator = tf.keras.models.load_model('molgen_generator.h5', custom_objects={'swish': swish},compile=False)
+molgen.discriminator = tf.keras.models.load_model('molgen_discriminator.h5', custom_objects={'swish': swish},compile=False)
+molgen.is_fitted # need to override after loading to allow molgen.transform to proceed
+
+
+# #### decoding synthetic propagator trajectories
+
+# In[90]:
+
+
+assert molgen.is_fitted == True
+
+
+# In[91]:
+
+
+synth_traj_id = 1
+xyz_synth = xyz_scaler.inverse_transform(molgen.transform(synth_trajs_scaled[synth_traj_id])).reshape(-1, n_atoms, 3)
+
+
+# In[94]:
+
+
+with open('synth.xyz', 'w') as f:
+    for i in range(xyz_synth.shape[0]):
+        f.write('%d\n' % n_atoms)
+        f.write('\n')
+        for k in range(n_atoms):
+            f.write('%3s%17.9f%17.9f%17.9f\n' % ('C', xyz_synth[i][k][0]*10, xyz_synth[i][k][1]*10, xyz_synth[i][k][2]*10) ) # nm to Angstroms for xyz write
+
+
+# In[95]:
+
+
+traj_ca[0].save_pdb('synth.pdb')
+
+
+# In[96]:
+
+
+traj_ca_synth = copy.deepcopy(traj_ca)
+
+
+# In[97]:
+
+
+traj_synth_obj = md.load('synth.xyz', top='synth.pdb')
+
+
+# In[98]:
+
+
+traj_synth_obj.center_coordinates(mass_weighted=False)
+traj_synth_obj.superpose(traj_synth_obj[0])
+
+
+# In[99]:
+
+
+view = nv.show_mdtraj(traj_synth_obj)
+#view.component_0.clear_representations()
+view.component_0.add_ribbon(color='red', opacity=0.6)
+view
+
+
+# ## (4) comparison
+
+# ### thermodynamics (FES)
+
+# 1D PMF in leading SRV coordinate
+# 
+# adding one pseudo-count to each bin as regularization and avoiding infinite free energies
+# 
+
+# In[563]:
+
+
+# MD 1D PMF mean
+n_bins = 50
+pdf_MD, bin_edges = np.histogram(hde_coords[:,0], bins=n_bins, density=True)
+bin_mids = bin_edges[:-1]+0.5*(bin_edges[1]-bin_edges[0])
+
+pdf_MD += 1./(len(hde_coords[:,0])*(bin_edges[1]-bin_edges[0])) # adding one pseudo-count per bin
+pmf_MD = -np.log(pdf_MD)
+pmf_MD -= np.mean(pmf_MD[np.isfinite(pmf_MD)]) # optimal mutual least squares alignment
+
+
+# In[564]:
+
+
+# MD 1D PMF block averages
+pmf_MD_block = []
+n_blocks=5
+for i in range(n_blocks):
+    
+    data_i = np.array_split(hde_coords[:,0],n_blocks,axis=0)[i] #block splits
+    
+    pdf_MD_i, _ = np.histogram(data_i, bins=bin_edges, density=True)
+    pdf_MD_i += 1./(len(data_i)*(bin_edges[1]-bin_edges[0])) # adding one pseudo-count per bin
+    
+    pmf_MD_i = -np.log(pdf_MD_i)
+    pmf_MD_i -= np.mean(pmf_MD_i[np.isfinite(pmf_MD_i)]) # optimal mutual least squares alignment
+    pmf_MD_block.append(pmf_MD_i)
+    
+pmf_MD_block = np.array(pmf_MD_block)
+pmf_MD_stderr = np.std(pmf_MD_block, axis=0)/np.sqrt(n_blocks)
+
+
+# In[565]:
+
+
+# shifting global minimum to zero of optimally least squares aligned landscapes
+PMF_shift = min(pmf_MD)
+pmf_MD -= PMF_shift
+for i in range(n_blocks):
+    pmf_MD_block[i] -= PMF_shift
+
+
+# In[1]:
+
+
+# plotting full and block PMFs
+fig, ax = plt.subplots()
+for i in range(n_blocks):
+    ax.plot(bin_mids, pmf_MD_block[i])
+im = ax.plot(bin_mids, pmf_MD, color='black', linewidth=3)
+plt.xlabel(r'$\psi_1$')
+plt.ylabel(r'$G$ ($k_BT$)')
+plt.show
+
+
+# In[567]:
+
+
+# plotting full PMFs with 95% CI (1.96 standard deviations in standard error)
+fig, ax = plt.subplots()
+ax.plot(bin_mids, pmf_MD, color='blue')
+ax.fill_between(bin_mids, pmf_MD-1.96*pmf_MD_stderr, pmf_MD+1.96*pmf_MD_stderr, alpha=0.2, color='blue')
+plt.xlabel(r'$\psi_1$')
+plt.ylabel(r'$G$ ($k_BT$)')
+plt.show
+
+
+# In[568]:
+
+
+# LSS 1D PMF mean
+synth_trajs_CONCAT = np.zeros((0,))
+for i in range(n_traj):
+    synth_trajs_CONCAT = np.concatenate((synth_trajs_CONCAT,synth_trajs[i][:,0]), axis=0)
+pdf_LSS, _ = np.histogram(synth_trajs_CONCAT, bins=n_bins, density=True)
+
+pdf_LSS += 1./(len(synth_trajs_CONCAT)*(bin_edges[1]-bin_edges[0])) # adding one pseudo-count per bin
+pmf_LSS = -np.log(pdf_LSS)
+pmf_LSS -= np.mean(pmf_LSS[np.isfinite(pmf_LSS)]) # optimal mutual least squares alignment
+
+
+# In[569]:
+
+
+# LSS 1D PMF block averages
+pmf_LSS_block = []
+n_blocks=5
+for i in range(n_blocks):
+    
+    data_i = np.array_split(synth_trajs_CONCAT,n_blocks,axis=0)[i] #block splits
+    
+    pdf_LSS_i, _ = np.histogram(data_i, bins=bin_edges, density=True)
+    pdf_LSS_i += 1./(len(data_i)*(bin_edges[1]-bin_edges[0])) # adding one pseudo-count per bin
+    
+    pmf_LSS_i = -np.log(pdf_LSS_i)
+    pmf_LSS_i -= np.mean(pmf_LSS_i[np.isfinite(pmf_LSS_i)]) # optimal mutual least squares alignment
+    pmf_LSS_block.append(pmf_LSS_i)
+    
+pmf_LSS_block = np.array(pmf_LSS_block)
+pmf_LSS_stderr = np.std(pmf_LSS_block, axis=0)/np.sqrt(n_blocks)
+
+
+# In[570]:
+
+
+# shifting global minimum to zero of optimally least squares aligned landscapes
+PMF_shift = min(pmf_LSS)
+pmf_LSS -= PMF_shift
+for i in range(n_blocks):
+    pmf_LSS_block[i] -= PMF_shift
+
+
+# In[571]:
+
+
+fig, ax = plt.subplots()
+for i in range(n_blocks):
+    ax.plot(bin_mids, pmf_LSS_block[i])
+im = ax.plot(bin_mids, pmf_LSS, color='black', linewidth=3)
+plt.xlabel(r'$\psi_1$')
+plt.ylabel(r'$G$ ($k_BT$)')
+plt.show
+
+
+# In[572]:
+
+
+fig, ax = plt.subplots()
+ax.plot(bin_mids, pmf_LSS, color='orange')
+ax.fill_between(bin_mids, pmf_LSS-pmf_LSS_stderr, pmf_LSS+pmf_LSS_stderr, alpha=0.2, color='orange')
+plt.xlabel(r'$\psi_1$')
+plt.ylabel(r'$G$ ($k_BT$)')
+plt.show
+
+
+# In[574]:
+
+
+fig, ax = plt.subplots()
+ax.plot(bin_mids, pmf_MD, color='blue', label='MD')
+ax.fill_between(bin_mids, pmf_MD-1.96*pmf_MD_stderr, pmf_MD+1.96*pmf_MD_stderr, alpha=0.2, color='blue')
+ax.plot(bin_mids, pmf_LSS, color='orange', label='LSS')
+ax.fill_between(bin_mids, pmf_LSS-1.96*pmf_LSS_stderr, pmf_LSS+1.96*pmf_LSS_stderr, alpha=0.2, color='orange')
+plt.xlabel(r'$\psi_1$')
+plt.ylabel(r'$G$ ($k_BT$)')
+ax.legend()
+plt.show
+
+
+# ### kinetic (relaxation times)
+
+# In[302]:
+
+
+def compute_autocorr_time(signal, delays):
+    ac=[]
+    for j in delays:
+        if type(signal) is list:
+            z = np.concatenate([item[:] for item in signal])
+            x = np.concatenate([item[:-j] for item in signal])
+            y = np.concatenate([item[j:] for item in signal])
+        else:
+            z = signal[:]
+            x = signal[:-j]
+            y = signal[j:]
+        ac.append(np.mean(x*y, axis=0)/np.mean(z*z, axis=0))
+    return np.array(ac)
+
+
+# In[303]:
+
+
+# MD mean autocorrelation and implied relaxation time
+ac_MD = compute_autocorr_time(hde_coords[:,:dim_prop], [lag_prop])
+relax_MD = -lag_prop/np.log(ac_MD)*timestep/1E3
+
+
+# In[304]:
+
+
+# MD block average standard error estimate
+ac_MD_block = []
+n_blocks=5
+for i in range(n_blocks):
+    ac_MD_i = compute_autocorr_time(np.array_split(hde_coords[:,:dim_prop],n_blocks,axis=0)[i], [lag_prop])
+    ac_MD_block.append(ac_MD_i)
+ac_MD_block = np.array(ac_MD_block)
+ac_MD_block = ac_MD_block.reshape(n_blocks,dim_prop)
+
+relax_MD_block = -lag_prop/np.log(ac_MD_block)*timestep/1E3
+relax_MD_stderr = np.std(relax_MD_block, axis=0)/np.sqrt(n_blocks)
+
+
+# In[305]:
+
+
+print('MD relaxation times (ns) = ')
+for i in range(relax_MD.shape[1]):
+    print('%f +/- %f' % (relax_MD[0,i], relax_MD_stderr[i]))
+
+
+# In[306]:
+
+
+# LSS mean autocorrelation and implied relaxation time
+ac_LSS = compute_autocorr_time(synth_trajs, [1])
+relax_LSS = -lag_prop/np.log(ac_LSS)*timestep/1E3
+
+
+# In[575]:
+
+
+# LSS block average standard error estimate
+# N.B. Blocks in this case are over complete indepenent LSS trajectories since data must be continuous
+
+assert n_traj >= 5 # need at least 5 independent trajectories for reasonable blocks
+
+ac_LSS_block = []
+for i in range(n_traj):
+    ac_LSS_i = compute_autocorr_time(synth_trajs[i], [1])
+    ac_LSS_block.append(ac_LSS_i)
+ac_LSS_block = np.array(ac_LSS_block)
+ac_LSS_block = ac_LSS_block.reshape(n_traj,synth_trajs[0].shape[1])
+
+relax_LSS_block = -lag_prop/np.log(ac_LSS_block)*timestep/1E3
+relax_LSS_stderr = np.std(relax_LSS_block, axis=0)/np.sqrt(n_traj)
+
+
+# In[576]:
+
+
+print('LSS relaxation times (ns) = ')
+for i in range(relax_LSS.shape[1]):
+    print('%f +/- %f' % (relax_synth[0,i], relax_synth_stderr[i]))
+
+
+# ### structure (RMSD)
+TO DO: 
+Follow protocol in Chem. Sci., 2020, 11, 9459 (77.pdf) - "Structures" on p. 9464
+
+- extract MD and LSS structures in the metastable basins and compare their relative Calpha RMSD using mdtraj or MDAnalysis libraries; note that this is a pairwise calculation -- given N_MD structures and N_LSS structures within a particular metastable basin, what is the mean value of RMSD_ij where i=1..N_MD and j=1..N_LSS
+
+- for this protein system we have two metastable states at \psi_1 ~ (-1.3) and (+0.7) so we should analyze the structures in both these basins by extracting snapshots from the MD and LSS trajectories with \psi_1 values of, say, [-1.4, -1.2] and [0.6, 0.8]. Depending on how many frames you extract, you may wish to loosen or tighten these bounds.
+# In[1]:
+
+
+import MDAnalysis
+
+
+# In[4]:
+
+
+MDAnalysis.rms.RMSD([0,1],[0,1])
+
+
+# In[5]:
+
+
+md.join(t,t)
+
+
+# In[ ]:
+
+
+
+
+'''
